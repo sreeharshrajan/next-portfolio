4bccfd6086d977377b94e91b4fb04ae28acaaba4
sreeharshrajan <sreeharshkrajan@gmail.com>
2024-02-17 17:08:13 +0530

    added checkly

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -11,26 +11,6 @@ export default function CTA() {
       } fixed inset-x-0 bottom-5 mx-5 flex max-w-screen-xl flex-col items-center justify-between space-y-3 rounded-lg border-t-4 border-black bg-white px-5 pb-3 pt-0 drop-shadow-lg transition-all duration-150 ease-in-out dark:border dark:border-t-4 dark:border-stone-700 dark:bg-black dark:text-white
           lg:flex-row lg:space-y-0 lg:pt-3 xl:mx-auto`}
     >
-      <button
-        onClick={() => setCloseCTA(!closeCTA)}
-        className={`${
-          closeCTA ? "rotate-180" : "rotate-0"
-        } absolute right-3 top-2 text-black transition-all duration-150 ease-in-out dark:text-white lg:hidden`}
-      >
-        <svg
-          viewBox="0 0 24 24"
-          width="30"
-          height="30"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          fill="none"
-          shapeRendering="geometricPrecision"
-        >
-          <path d="M6 9l6 6 6-6" />
-        </svg>
-      </button>
       <div className="text-center lg:text-left">
         <p className="font-title text-lg text-black dark:text-white sm:text-2xl">
           Platforms Starter Kit Demo
