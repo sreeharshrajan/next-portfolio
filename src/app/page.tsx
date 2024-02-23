2130bd7bca3cb77b49325c8c4fe2d67d6be13b83
sreeharshrajan <sreeharshkrajan@gmail.com>
2024-02-23 19:53:25 +0530

    added animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Hero from "@/components/blocks/Hero";
 import Image from "next/image";
 
+import "@/assets/styles/globals.css";
+
 export default function Home() {
   return (
     <main className="w-full overflow-hidden">
--- src/components/blocks/Hero/index.tsx
+++ src/components/blocks/Hero/index.tsx
@@ -1,61 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { annotate } from "rough-notation";
 import { motion } from "framer-motion";
 import { fadeIn } from "@utils/animate";
 
-import '@/assets/styles/globals.css'
-
 const Hero = () => {
   const [playAnimation, setPlayAnimation] = useState(true);
-  useEffect(() => {
-    const top = document.getElementById("top");
-
-    const e = document.querySelector("#hero-text");
-    if (e) {
-      setTimeout(() => {
-        annotate(e, {
-          type: "highlight",
-          color: "#ffda79",
-          multiline: true,
-        }).show();
-      }, 2000);
-    }
-  }, []);
   return (
     <div className="relative overflow-hidden min-h-screen min-w-screen">
-      <motion.div
-        variants={fadeIn("down", "tween", 0.5, 1)}
-        whileInView={`show`}
-        initial={`hidden`}
-        className="flex items-center max-sm:px-2 justify-center"
-      >
-        <h1 id="top" className="color_box">
-          Over 3 million ready-to-work creatives in our community!
-        </h1>
-      </motion.div>
-      <motion.div
-        variants={fadeIn("tween", 1, 1)}
-        whileInView={`show`}
-        initial={`hidden`}
-        className=""
-      >
-        <h1 className="hero-text w-1/2">
-          Hire the <span id="hero-text">world’s top</span> creative talent.
-        </h1>
-      </motion.div>
-      <motion.div
-        variants={fadeIn("up", "tween", 1.5, 0.7)}
-        whileInView={`show`}
-        initial={`hidden`}
-        className="flex justify-center max-sm:px-12"
-      >
-        <h2 className="text-[20px] max-sm:text-[15px] text-[#0d0c22] text-center font-poppins rounded-full">
-          Connect with a community of millions of top-rated designers & agencies
-          around the world.
-        </h2>
-      </motion.div>
       <div
         aria-hidden="true"
         className="flex absolute -top-96  start-1/2 transform -translate-x-1/2"
@@ -67,25 +19,44 @@ const Hero = () => {
       <div className="relative z-10 flex flex-col justify-center items-center h-screen">
         <div className="max-w-xl mx-auto px-4">
           <div className="max-w-2xl text-center mx-auto">
-            <p className="inline-block text-xl font-bold bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
+            <motion.p
+              variants={fadeIn("down", "tween", 0.5, 1)}
+              whileInView={`show`}
+              initial={`hidden`}
+              className="inline-block text-xl font-bold bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400"
+            >
               Hi, I am Sreeharsh
-            </p>
+            </motion.p>
 
             <div className="mt-5 max-w-2xl">
-              <h1 className="block font-semibold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
-                Full Stack <br /> Developer
-              </h1>
+              <motion.h1
+                variants={fadeIn("tween", 1, 1)}
+                whileInView={`show`}
+                initial={`hidden`}
+                className="block font-semibold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200"
+              >
+                Full Stack <br />
+                Developer
+              </motion.h1>
             </div>
 
-            <div className="mt-5 max-w-3xl">
-              <p className="text-lg text-gray-600 dark:text-gray-400">
+            <motion.div
+              variants={fadeIn("up", "tween", 1.5, 0.7)}
+              whileInView={`show`}
+              initial={`hidden`}
+              className="mt-5 max-w-3xl"
+            >
+              <p className="text-lg  text-gray-600 dark:text-gray-400">
                 Empowering Your Ideas Through <br /> Development Expertise and
                 Seamless User Interfaces
               </p>
-            </div>
+            </motion.div>
 
             <div className="mt-8 gap-3 flex justify-center">
-              <a
+              <motion.a
+                variants={fadeIn("up", "tween", 2.0, 1)}
+                whileInView={`show`}
+                initial={`hidden`}
                 className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 href="/assets/files/Sreeharsh_Resume.pdf"
                 download
@@ -105,7 +76,7 @@ const Hero = () => {
                 >
                   <path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5" />
                 </svg>
-              </a>
+              </motion.a>
             </div>
           </div>
         </div>
