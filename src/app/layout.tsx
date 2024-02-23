4da4dcc2d74619fc8435b7e00b937d0c2d46e3f5
sreeharshrajan <sreeharshkrajan@gmail.com>
2024-02-23 23:28:06 +0530

    Added Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Navbar from "@components/layout/Navbar";
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights/>A
       </body>
     </html>
   );
