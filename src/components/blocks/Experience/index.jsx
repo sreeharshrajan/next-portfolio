207b8cb255173fd8e00743d9ee1b19d13eb696d0
sreeharshrajan <sreeharshkrajan@gmail.com>
2024-03-22 21:47:14 +0530

    fix

--- src/components/blocks/Experience/index.jsx
+++ src/components/blocks/Experience/index.jsx
@@ -12,7 +12,7 @@ const Experience = () => {
   return (
     <section
       id="experience"
-      className="lg:my-20 w-11/12 md:w-10/12 lg:w-4/6 xl:w-4/6 py-11 space-y-20 min-h-lvh"
+      className="lg:my-20 w-11/12 md:w-10/12 lg:w-4/6 xl:w-4/6 py-11 space-y-10 md:space-y-20 md:min-h-lvh"
       ref={container}
     >
       <motion.h3
